7be2ae1e0a47ffdd2f495319eaf90a82830fa747
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-02-16 15:25:17 +0900

    [fix] BE時のエラーの解消、観戦選択対象の修正

--- src/main/kotlin/com/github/tanokun/spec/ui/button/PlayerButton.kt
+++ src/main/kotlin/com/github/tanokun/spec/ui/button/PlayerButton.kt
@@ -12,32 +12,31 @@ import org.bukkit.entity.Player
 import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.InventoryClickEvent
 import xyz.xenondevs.invui.item.ItemProvider
+import xyz.xenondevs.invui.item.builder.AbstractItemBuilder
 import xyz.xenondevs.invui.item.builder.SkullBuilder
 import xyz.xenondevs.invui.item.impl.AbstractItem
 
-class PlayerButton(private val player: Player, private val selectUI: SelectUI): AbstractItem() {
+private const val WHEN_BE_HEAD_TEXTURE = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmQyMDY1ZjJkMjA2YzBiNWQwMmMzNzY5ZGVmYzg5ZWU5YzUwMGZjMjYwMGY4NzA1NmQxYWEwNDk4NmRjZWEyMSJ9fX0="
+
+class PlayerButton(private val target: Player, private val selectUI: SelectUI): AbstractItem() {
 
     override fun getItemProvider(): ItemProvider {
-        if (PlayerFilter.CurrentSpectator.filter(player)) {
-            return SkullBuilder(SkullBuilder.HeadTexture.of(player))
-                .setDisplayName("§b§l${player.name}")
-                .addLoreLines(
-                    "§7現在の参加数: ${player.asJinrouPlayer().getTotalPlayer()}回",
-                    "§7現在の観戦数: ${player.asJinrouPlayer().getTotalSpectator()}回",
-                    "",
-                    "§7クリックで、「§l参加者§7」にします。"
-                )
+        val builder: AbstractItemBuilder<SkullBuilder> = try {
+            SkullBuilder(SkullBuilder.HeadTexture.of(target))
+        } catch (_: Exception) {
+            SkullBuilder(SkullBuilder.HeadTexture(WHEN_BE_HEAD_TEXTURE))
+        }
+
+        if (PlayerFilter.CurrentSpectator.filter(target)) {
+            return builder
+                .setDisplayName("§b§l${target.name}")
+                .addLoreLines(*lore("§l参加者"))
                 .addAllItemFlags()
         }
 
-        return SkullBuilder(SkullBuilder.HeadTexture.of(player))
-            .setDisplayName("§l${player.name}")
-            .addLoreLines(
-                "§7現在の参加数: ${player.asJinrouPlayer().getTotalPlayer()}回",
-                "§7現在の観戦数: ${player.asJinrouPlayer().getTotalSpectator()}回",
-                "",
-                "§7クリックで、「§b§l観戦者§7」にします。"
-            )
+        return builder
+            .setDisplayName("§l${target.name}")
+            .addLoreLines(*lore("§b§l観戦者"))
             .addAllItemFlags()
     }
 
@@ -47,9 +46,18 @@ class PlayerButton(private val player: Player, private val selectUI: SelectUI): 
 
         player.playSound(player, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0F, 1.0F)
 
-        if (PlayerFilter.CurrentSpectator.filter(player)) player.unregisterPlayer()
-        else player.registerSpectator()
+        if (PlayerFilter.CurrentSpectator.filter(player)) target.unregisterPlayer()
+        else target.registerSpectator()
 
         selectUI.update()
     }
+
+    private fun lore(next: String) =
+        arrayOf(
+            "§7現在の参加数: ${target.asJinrouPlayer().getTotalPlayer()}回",
+            "§7現在の観戦数: ${target.asJinrouPlayer().getTotalSpectator()}回",
+            "",
+            "§7クリックで、「$next§7」にします。"
+        )
+
 }
