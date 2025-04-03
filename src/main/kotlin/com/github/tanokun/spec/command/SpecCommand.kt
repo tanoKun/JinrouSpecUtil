e26f6416f862531a271539a9fb998fd3e1407335
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-04-03 23:17:59 +0900

    [fix] プレイヤーヘッド取得を非同期に

--- src/main/kotlin/com/github/tanokun/spec/command/SpecCommand.kt
+++ src/main/kotlin/com/github/tanokun/spec/command/SpecCommand.kt
@@ -1,6 +1,8 @@
 package com.github.tanokun.spec.command
 
+import com.github.tanokun.spec.plugin
 import com.github.tanokun.spec.ui.SelectUI
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -9,11 +11,15 @@ import java.util.*
 
 object SpecCommand: CommandExecutor {
     private val selectUIs = HashMap<UUID, SelectUI>()
+
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender !is Player) return false
 
-        val selectUI = selectUIs.getOrPut(sender.uniqueId) { SelectUI(sender.uniqueId) }
-        selectUI.open()
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable {
+            val selectUI = selectUIs.getOrPut(sender.uniqueId) { SelectUI(sender.uniqueId) }
+
+            Bukkit.getScheduler().callSyncMethod(plugin) { selectUI.open() }
+        })
 
         return true
     }
