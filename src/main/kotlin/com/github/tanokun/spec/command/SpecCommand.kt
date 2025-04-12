fa50fe4e13624c8596bd07acf74b6dea2ada5825
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-04-12 22:47:37 +0900

    [fix] 取得部のキャッシュ化

--- src/main/kotlin/com/github/tanokun/spec/command/SpecCommand.kt
+++ src/main/kotlin/com/github/tanokun/spec/command/SpecCommand.kt
@@ -2,11 +2,14 @@ package com.github.tanokun.spec.command
 
 import com.github.tanokun.spec.plugin
 import com.github.tanokun.spec.ui.SelectUI
+import com.github.tanokun.spec.ui.button.WHEN_BE_HEAD_TEXTURE
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import xyz.xenondevs.invui.item.builder.AbstractItemBuilder
+import xyz.xenondevs.invui.item.builder.SkullBuilder
 import java.util.*
 
 object SpecCommand: CommandExecutor {
@@ -16,6 +19,15 @@ object SpecCommand: CommandExecutor {
         if (sender !is Player) return false
 
         Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable {
+            //caching
+            Bukkit.getOnlinePlayers().forEach {
+                try {
+                    SkullBuilder.HeadTexture.of(it)
+                } catch (_: Exception) {
+                    SkullBuilder.HeadTexture(WHEN_BE_HEAD_TEXTURE)
+                }
+            }
+
             val selectUI = selectUIs.getOrPut(sender.uniqueId) { SelectUI(sender.uniqueId) }
 
             Bukkit.getScheduler().callSyncMethod(plugin) { selectUI.open() }
